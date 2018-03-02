a7944aba7fd36282bf28773a265ababe8086a0db
Raheel Ahmad <ahmadraheel@gmail.com>
2018-03-01 19:07:20 -0800

    Add tables

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,32 @@
 CREATE DATABASE marcone;
 \c marcone;
 
-CREATE TABLE podcasts (
+CREATE TABLE episodes (
+  episode_id text PRIMARY KEY,
   title text,
-  author_name text
+  description text,
+  pub_date date,
+  guid text,
+  image_url text,
+  duration integer,
+  enclosure_type text,
+  enclosure_length text,
+  enclosure_url text
 );
-INSERT INTO podcasts (title, author_name) VALUES ('This American Life', 'NPR');
-INSERT INTO podcasts (title, author_name) VALUES ('Back to Work', '5by5');
+
+CREATE TABLE podcasts (
+  url text PRIMARY KEY,
+  title text,
+  description text,
+  author_name text,
+  copyright text,
+  image_url text,
+  author text,
+  category text,
+  type text,
+
+  episode_id text REFERENCES episodes
+);
+
+-- INSERT INTO podcasts (title, author_name) VALUES ('This American Life', 'NPR');
+-- INSERT INTO podcasts (title, author_name) VALUES ('Back to Work', '5by5');
