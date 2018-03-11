0c015b5a73f8656098a4ce2e4ff3824fe26b3c54
Raheel Ahmad <ahmadraheel@gmail.com>
2018-03-11 00:09:54 -0700

    Add seed URLs

--- Sources/marcone/FeedRefreshController.swift
+++ Sources/marcone/FeedRefreshController.swift
@@ -10,6 +10,20 @@ import Vapor
 import Console
 import marconeLib
 
+let seedURLs = [
+    "http://feeds.gimletmedia.com/hearreplyall",
+    "http://feed.thisamericanlife.org/talpodcast",
+    "http://www.standup.fm/rss",
+    "http://kadavy.libsyn.com/rss",
+    "https://rss.art19.com/good-life-project",
+    "http://feeds.gimletmedia.com/uncivil",
+    "https://huffduffer.com/raheel/rss",
+    "http://exponent.fm/feed/",
+    "https://www.relay.fm/radar/feed",
+    "http://feeds.feedburner.com/Monocle24CultureWithRobBound",
+    "http://trackchanges.libsyn.com/rss",
+]
+
 final class FeedRefreshCommand: Command, ConfigInitializable {
     let id = "refresh"
 
@@ -24,7 +38,10 @@ final class FeedRefreshCommand: Command, ConfigInitializable {
     func run(arguments: [String]) throws {
         log.info("Doing an hourly task")
         let urls = try PodcastsController.allURLs()
-        for url in urls {
+
+        let missingURLs = Set(urls).union(seedURLs)
+
+        for url in missingURLs {
             drop?.log.info("Will fetch from \(url)")
             try PodcastsController.addOrUpdate(fromURL: url)
         }
